e360a58097abf9e4fc99009e5d79055a5254f063
pavvyb <pavvyb@icloud.com>
2020-02-28 12:42:58 +0300

    dbdbdb

--- main.py
+++ main.py
@@ -48,10 +48,6 @@ json_body = [
 
 pathh = "/home/popufey/Desktop/lab/xentop.1581649624"
 
-client.write_points(json_body)
-result = client.query('SELECT "duration" FROM "labtest"."autogen"."brushEvents" WHERE time > now() - 10000d GROUP BY "user"')
-print("Result: {0}".format(result))
-
 def convertTime(unixformat):
     standardform = datetime.utcfromtimestamp(int(unixformat.split(".")[1])).strftime('%Y-%m-%d %H:%M:%S')
     # optional
@@ -84,3 +80,76 @@ def extractData(path):
     return dataArr
 
 toInsert = extractData(pathh)
+
+print("to insert")
+print(toInsert)
+
+def prepareObject(unprepared):
+    dataRes = []
+    dataRes.append(
+        {
+            "measurement": "devices",
+            "tags": {
+                "user": "load",
+                "brushId": unprepared[16]
+            },
+            "time": unprepared[-1],
+            "fields": {
+                "CPUsec": unprepared[0],
+                "CPUper": unprepared[1],
+                "MEMk": unprepared[2],
+                "MEMper": unprepared[3],
+                "MAXMEMk": unprepared[4],
+                "MAXMEMper": unprepared[5],
+                "VCPUS": unprepared[6],
+                "NETS": unprepared[7],
+                "NETTX": unprepared[8],
+                "NETRX": unprepared[9],
+                "VBDS": unprepared[10],
+                "VBD_OO": unprepared[11],
+                "VBD_RD": unprepared[12],
+                "VBD_WR": unprepared[13],
+                "VBD_RSECT": unprepared[14],
+                "VBD_WSECT": unprepared[15],
+                "SSID": unprepared[16]
+            }
+        }
+    )
+    # measurement = "devices"
+    # tags = {"user": "load",
+    #         "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"}
+    # time = unprepared[-1]
+    # fields = {}
+    # fields["CPUsec"] = unprepared[0]
+    # fields["CPUper"] = unprepared[1]
+    # fields["MEMk"] = unprepared[2]
+    # fields["MEMper"] = unprepared[3]
+    # fields["MAXMEMk"] = unprepared[4]
+    # fields["MAXMEMper"] = unprepared[5]
+    # fields["VCPUS"] = unprepared[6]
+    # fields["NETS"] = unprepared[7]
+    # fields["NETTX"] = unprepared[8]
+    # fields["NETRX"] = unprepared[9]
+    # fields["VBDS"] = unprepared[10]
+    # fields["VBD_OO"] = unprepared[11]
+    # fields["VBD_RD"] = unprepared[12]
+    # fields["VBD_WR"] = unprepared[13]
+    # fields["VBD_RSECT"] = unprepared[14]
+    # fields["VBD_WSECT"] = unprepared[15]
+    # fields["SSID"] = unprepared[16]
+    # print("FIELDS")
+    # print(fields)
+    # resultObject = {}
+    # resultObject["measurement"] = measurement
+    # resultObject["tags"] = tags
+    # resultObject["time"] = time
+    # resultObject["fields"] = fields
+    return dataRes
+
+ressss = prepareObject(toInsert)
+
+
+client.write_points(ressss)
+print("DATA INSERTED TO DB")
+result = client.query('SELECT * FROM "labtest"."autogen"."devices" WHERE time > now() - 100000d GROUP BY "user"')
+print("Result: {0}".format(result))
