198abff00dd415736176b2a4595137bc1dd512b1
popufey <pavvyb@icloud.com>
2020-04-20 16:00:14 +0400

    very big fixes

--- main.py
+++ main.py
@@ -6,48 +6,10 @@ from os import listdir
 from os.path import isfile, join
 
 client = InfluxDBClient(host='localhost', port=8086)
-client.create_database('labtest')
+client.create_database('newtest')
 dbaselist = client.get_list_database()
 print("Databases:", dbaselist)
-client.switch_database('labtest')
-
-# fields = ["CPUsec","CPUper","MEMk", "MEMper", "MAXMEMk", "MAXMEMper", "VCPUS", "NETS", "NETTX", "NETRX", "VBDS", "VBD_OO", "VBD_RD", "VBD_WR", "VBD_RSECT", "VBD_WSECT", "SSID"]
-#
-# json_body = [
-#     {
-#         "measurement": "brushEvents",
-#         "tags": {
-#             "user": "load",
-#             "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-#         },
-#         "time": "2018-03-28T8:01:00Z",
-#         "fields": {
-#             "duration": 127
-#         }
-#     },
-#     {
-#         "measurement": "brushEvents",
-#         "tags": {
-#             "user": "load",
-#             "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-#         },
-#         "time": "2018-03-29T8:04:00Z",
-#         "fields": {
-#             "duration": 132
-#         }
-#     },
-#     {
-#         "measurement": "brushEvents",
-#         "tags": {
-#             "user": "load",
-#             "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-#         },
-#         "time": "2018-03-30T8:02:00Z",
-#         "fields": {
-#             "duration": 129
-#         }
-#     }
-# ]
+client.switch_database('newtest')
 
 pathh = "/home/popufey/Desktop/proj3rdyear/xentop.1581649624"
 
@@ -137,101 +99,129 @@ for i in allFilepaths:
     print(toInsert)
     metricArray.append(toInsert)
     ressss = prepareObject(toInsert)
-    # client.write_points(ressss)
+    print(ressss)
+    client.write_points(ressss)
     print("DATA INSERTED TO DB")
-xxx = input("Enter")
-print(xxx)
-# Arrays for all metrics
-metric1 = []
-metric2 = []
-metric3 = []
-metric4 = []
-metric5 = []
-metric6 = []
-metric7 = []
-metric8 = []
-metric9 = []
-metric10 = []
-metric11 = []
-metric12 = []
-metric13 = []
-metric14 = []
-metric15 = []
-metric16 = []
-metric17 = []
 
-# Insering data to metrics arrays
-for i in metricArray:
-    metric1.append(float(i[0]))
-    metric2.append(float(i[1]))
-    metric3.append(float(i[2]))
-    metric4.append(float(i[3]))
-    metric5.append(float(i[4]))
-    metric6.append(float(i[5]))
-    metric7.append(float(i[6]))
-    metric8.append(float(i[7]))
-    metric9.append(float(i[8]))
-    metric10.append(float(i[9]))
-    metric11.append(float(i[10]))
-    metric12.append(float(i[11]))
-    metric13.append(float(i[12]))
-    metric14.append(float(i[13]))
-    metric15.append(float(i[14]))
-    metric16.append(float(i[15]))
-    metric17.append(float(i[16]))
-
-# Final arrays for metrics
-finalmetric1 = ["CPUsec", sparklines(metric1)]
-finalmetric2 = ["CPUper", sparklines(metric2)]
-finalmetric3 = ["MEMk", sparklines(metric3)]
-finalmetric4 = ["MEMper", sparklines(metric4)]
-finalmetric5 = ["MAXMEMk", sparklines(metric5)]
-finalmetric6 = ["MAXMEMper", sparklines(metric6)]
-finalmetric7 = ["VCPUS", sparklines(metric7)]
-finalmetric8 = ["NETS", sparklines(metric8)]
-finalmetric9 = ["NETTX", sparklines(metric9)]
-finalmetric10 = ["NETRX", sparklines(metric10)]
-finalmetric11 = ["VBDS", sparklines(metric11)]
-finalmetric12 = ["VBD_OO", sparklines(metric12)]
-finalmetric13 = ["VBD_RD", sparklines(metric13)]
-finalmetric14 = ["VBD_WR", sparklines(metric14)]
-finalmetric15 = ["VBD_RSECT", sparklines(metric15)]
-finalmetric16 = ["VBD_WSECT", sparklines(metric16)]
-finalmetric17 = ["SSID", sparklines(metric17)]
-
-# result = client.query('SELECT * FROM "labtest"."autogen"."devices" WHERE time > now() - 100000d GROUP BY "user"')
-# print("Result: {0}".format(result))
-
-# Array of pairs [metric, sparkline]
-metricArray = []
-metricArray.append(finalmetric1)
-metricArray.append(finalmetric2)
-metricArray.append(finalmetric3)
-metricArray.append(finalmetric4)
-metricArray.append(finalmetric5)
-metricArray.append(finalmetric6)
-metricArray.append(finalmetric7)
-metricArray.append(finalmetric8)
-metricArray.append(finalmetric9)
-metricArray.append(finalmetric10)
-metricArray.append(finalmetric11)
-metricArray.append(finalmetric12)
-metricArray.append(finalmetric13)
-metricArray.append(finalmetric14)
-metricArray.append(finalmetric15)
-metricArray.append(finalmetric16)
-metricArray.append(finalmetric17)
 
 x = ["Metric 1", sparklines([1, 2, 3, 4, 5.0, 9, 3, 2, 1, 434, 341, 262, 133, 3, 2, 1, 1,2, 1, 1, 16, 22, 4, 42])]
 y = ["Metric 2", sparklines([133, 242, 421, 423, 542, 434, 341, 262, 133, 3, 2, 1, 1, 16, 22, 4, 42, 1, 2, 3, 4, 5.0, 9, 3, 2, 1])]
 z = ["Metric 3", sparklines([1, 16, 22, 4, 42, 12, 3, 133, 242, 421, 423, 542, 434, 341, 262, 133, 3, 2, 1, 1, 16, 22, 4,  12, 3, 133, 242, 421, 42, 12, 3, 2, 1])]
 metrics = [x,y,z]
 
-print("Testing the sparklines for an existing metrics")
-for item in metricArray:
-    print(item[0], item[1][0])
+def findMin(inpArr):
+    mini = inpArr[0]
+    for i in inpArr:
+        if (i < mini):
+            mini = i
+    return mini
+
+def correctMin(inpArr):
+    mii = findMin(inpArr)
+    for i in inpArr:
+        i = i - mii
+    return inpArr
+def makeSparksNew(inpArr):
+    outpArr = []
+
+    # Arrays for all metrics
+    metric1 = []
+    metric2 = []
+    metric3 = []
+    metric4 = []
+    metric5 = []
+    metric6 = []
+    metric7 = []
+    metric8 = []
+    metric9 = []
+    metric10 = []
+    metric11 = []
+    metric12 = []
+    metric13 = []
+    metric14 = []
+    metric15 = []
+    metric16 = []
+    metric17 = []
+    # Insering data to metrics arrays
+    for i in inpArr:
+        metric1.append(float(i['CPUsec']))
+        metric2.append(float(i["CPUper"]))
+        metric3.append(float(i['MEMk']))
+        metric4.append(float(i['MEMper']))
+        metric5.append(float(i['MAXMEMk']))
+        metric6.append(float(i['MAXMEMper']))
+        metric7.append(float(i['VCPUS']))
+        metric8.append(float(i['NETS']))
+        metric9.append(float(i['NETTX']))
+        metric10.append(float(i['NETRX']))
+        metric11.append(float(i['VBDS']))
+        metric12.append(float(i['VBD_OO']))
+        metric13.append(float(i['VBD_RD']))
+        metric14.append(float(i['VBD_WR']))
+        metric15.append(float(i['VBD_RSECT']))
+        metric16.append(float(i['VBD_WSECT']))
+        metric17.append(float(i['SSID']))
+
+    # Correctiong minimals
+    metric1 = correctMin(metric1)
+    metric2 = correctMin(metric2)
+    metric3 = correctMin(metric3)
+    metric4 = correctMin(metric4)
+    metric5 = correctMin(metric5)
+    metric6 = correctMin(metric6)
+    metric7 = correctMin(metric7)
+    metric8 = correctMin(metric8)
+    metric9 = correctMin(metric9)
+    metric10 = correctMin(metric10)
+    metric11 = correctMin(metric11)
+    metric12 = correctMin(metric12)
+    metric13 = correctMin(metric13)
+    metric14 = correctMin(metric14)
+    metric15 = correctMin(metric15)
+    metric16 = correctMin(metric16)
+    metric17 = correctMin(metric17)
 
 
+    # Final arrays for metrics
+    finalmetric1 = ["CPUsec", sparklines(metric1)]
+    finalmetric2 = ["CPUper", sparklines(metric2)]
+    finalmetric3 = ["MEMk", sparklines(metric3)]
+    finalmetric4 = ["MEMper", sparklines(metric4)]
+    finalmetric5 = ["MAXMEMk", sparklines(metric5)]
+    finalmetric6 = ["MAXMEMper", sparklines(metric6)]
+    finalmetric7 = ["VCPUS", sparklines(metric7)]
+    finalmetric8 = ["NETS", sparklines(metric8)]
+    finalmetric9 = ["NETTX", sparklines(metric9)]
+    finalmetric10 = ["NETRX", sparklines(metric10)]
+    finalmetric11 = ["VBDS", sparklines(metric11)]
+    finalmetric12 = ["VBD_OO", sparklines(metric12)]
+    finalmetric13 = ["VBD_RD", sparklines(metric13)]
+    finalmetric14 = ["VBD_WR", sparklines(metric14)]
+    finalmetric15 = ["VBD_RSECT", sparklines(metric15)]
+    finalmetric16 = ["VBD_WSECT", sparklines(metric16)]
+    finalmetric17 = ["SSID", sparklines(metric17)]
+
+    # Array of pairs [metric, sparkline]
+    outpArr = []
+    outpArr.append(finalmetric1)
+    outpArr.append(finalmetric2)
+    outpArr.append(finalmetric3)
+    outpArr.append(finalmetric4)
+    outpArr.append(finalmetric5)
+    outpArr.append(finalmetric6)
+    outpArr.append(finalmetric7)
+    outpArr.append(finalmetric8)
+    outpArr.append(finalmetric9)
+    outpArr.append(finalmetric10)
+    outpArr.append(finalmetric11)
+    outpArr.append(finalmetric12)
+    outpArr.append(finalmetric13)
+    outpArr.append(finalmetric14)
+    outpArr.append(finalmetric15)
+    outpArr.append(finalmetric16)
+    outpArr.append(finalmetric17)
+
+    return outpArr
 
 while (True):
     print('Specify the start date:')
@@ -252,9 +242,21 @@ while (True):
         endtime = '1:11:11'
     finalstart = startdate + 'T' + starttime + 'Z'
     finalend = enddate + 'T' + endtime + 'Z'
-    requestbd = "SELECT * FROM labtest WHERE time >= '" + finalstart + "' AND time <= '" + finalend + "'"
+    finalend = "'"+finalend+"'"
+    finalstart = "'"+finalstart+"'"
+    requestbd = 'SELECT * FROM "newtest"."autogen"."devices" WHERE time >= '+ finalstart + ' AND time <= ' + finalend + 'ORDER BY time ASC'
+    # result = client.query('SELECT * FROM "newtest"."autogen"."devices" WHERE time > now() - 100000d GROUP BY "user"')
     result = client.query(requestbd)
-    for zz in result:
-        print(zz)
-    # print("Result: {0}".format(result))
-    #+ 'GROUP BY time(12m),* fill(200) LIMIT 7 SLIMIT 1'
+    resultset = []
+    print(requestbd)
+    for qus in result:
+        for qq in qus:
+            resultset.append(qq)
+    # for finn in resultset:
+    #     print(finn)
+    answerArray = makeSparksNew(resultset)
+
+
+    print("The Sparklines for this timeframe:")
+    for item in answerArray:
+        print(item[0], item[1][0])
